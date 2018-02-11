7c6eb9a13094a84e942fdcbc93b8a75a91775283
ryssalama <johansson.kristo@gmail.com>
2018-02-11 18:22:15 +0200

    s

---
+++ Assets/Scripts/ViliOnHomo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViliOnHomo : MonoBehaviour {
+
+	private string viliMessage;
+
+	void Start () {
+		PrintMessage ();
+	}
+
+	void PrintMessage() {
+		viliMessage = "Homo";
+		Debug.Log (viliMessage);
+	}
+}
