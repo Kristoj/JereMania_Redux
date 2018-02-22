ec8a55dd2d421307ac1abb13e1ce0931df4edea0
ryssalama <johansson.kristo@gmail.com>
2018-02-22 17:49:38 +0200

    ddwkowq

---
+++ Assets/Editor/CaveSpawnerEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(Cavespawner))]
+public class CaveSpawnerEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector ();
+		Cavespawner myScript = target as Cavespawner;
+
+		if (GUILayout.Button ("Generate Cave")) {
+			//myScript.caveSpawn ();
+		}
+
+		if (GUILayout.Button ("Clear Cave")) {
+			//myScript.ClearCave ();
+		}
+	}
+}
